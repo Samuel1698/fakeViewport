da06413161dfd024c34272e7f54a0464b7a252eb
SamFramework <samuel1698500@gmail.com>
2025-04-20 17:27:34 -0700

    remove spaces in monitoring.py

--- monitoring.py
+++ monitoring.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 # Set log level to Warning
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.WARNING)
-
 @app.route('/check_view')
 def api_check_view():
     # Construct the path to the file in the user's home directory
@@ -22,13 +21,11 @@ def api_check_view():
     with open(view_status_file, 'r') as f:
         result = f.read()
     return jsonify(view_status=result)
-
 @app.route('/get_system_uptime')
 def get_system_uptime():
     # Get the uptime in seconds
     uptime_seconds = uptime.uptime()
     return jsonify(system_uptime=uptime_seconds)
-
 @app.route('/get_script_uptime')
 def api_get_script_uptime():
     # Construct the path to the file in the user's home directory
@@ -38,18 +35,15 @@ def api_get_script_uptime():
     script_uptime = datetime.now() - script_start_time
     uptime_seconds = script_uptime.total_seconds()
     return jsonify(script_uptime=uptime_seconds)
-
 @app.route('/admin')
 def admin():
     view_status = api_check_view().json['view_status']
     script_uptime = api_get_script_uptime().json['script_uptime']
     system_uptime = get_system_uptime().json['system_uptime']
-
     return jsonify(
         view_status=view_status,
         script_uptime=script_uptime,
         system_uptime=system_uptime
     )
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
